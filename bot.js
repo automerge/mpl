7bc5194c67ce5939d0f02e84eface90f53885c4e
Peter van Hardenberg <pvh@pvh.ca>
2017-06-19 20:34:58 -0700

    consolidate to one bot from two, which works.

--- bot.js
+++ bot.js
@@ -1,23 +1,31 @@
 let ampl = require('./lib/ampl')
 let wrtc = require('wrtc')
 
+let fieldName = (process.env.NAME || 'anonybot') + Math.floor(Math.random() * 1000)
+
 let store = new ampl.default.Store(
     (state, action) => {
       switch(action.type) {
         case "INCREMENT_COUNTER":
           return ampl.default.Tesseract.changeset(state, "increment counter", (doc) => {
-            doc.counter = (state.counter || 0) + 1
+            doc[fieldName] = (state[fieldName] || 0) + 1
           })
         default:
           return state
       }
-    },
-    // for bots we need to pass in a network that has a node-friendly webrtc implementation.
-    new ampl.default.Network({wrtc: wrtc}))
+    }, new ampl.default.Network(wrtc))
 
 store.dispatch({ type: "OPEN_DOCUMENT", docId: "botcounter" })
 
-//setInterval( () => store.dispatch({ type: "INCREMENT_COUNTER", docId: "botcounter" }), 5000)
+if (process.env.REMOTEHOST) {
+  store.network.signaler.manualHello(process.env.REMOTEHOST, process.env.REMOTEPORT)
+}
+
+setInterval( () => store.dispatch({ type: "INCREMENT_COUNTER", docId: "botcounter" }), 5000)
+
+store.network.peergroup.on('peer', (peer) => {
+    console.log("New peer:", peer)
+})
 
 store.subscribe( () => {
     var state = store.getState()

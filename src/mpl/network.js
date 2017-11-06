4bc1804574a56b7209d1ac82d6abbbbd96243b05
Peter van Hardenberg <pvh@pvh.ca>
2017-11-06 12:41:04 -0800

    remove peergroup

--- src/mpl/network.js
+++ src/mpl/network.js
@@ -1,6 +1,5 @@
 import PeerStats from './network/peer-stats'
 
-import PeerGroup from './network/peergroup'
 import EventEmitter from 'events'
 import config from './config'
 
@@ -8,8 +7,21 @@ export default class Network extends EventEmitter {
   constructor(docSet, wrtc) {
     super()
 
-    this.peergroup = new PeerGroup(docSet, wrtc)
-    this.peerStats = new PeerStats(this.peergroup)
+    const ipfs = new IPFS({
+      repo: 'ipfs/pubsub-demo/' + Math.random(),
+      EXPERIMENTAL: {
+        pubsub: true
+      },
+      config: {
+        "Addresses": {
+          "API": "",
+          "Gateway": "",
+          "Swarm": [
+            "/ip4/0.0.0.0/tcp/0",
+        ]}}
+    })
+
+    this.ipfs = ipfs
 
     this.connected = false
   }
@@ -21,13 +33,50 @@ export default class Network extends EventEmitter {
     this.config = config || this.config
 
     let name   = this.config.name || process.env.NAME
-    let peerId = this.config.peerId
+
     if (!peerId) throw new Error("peerId required, not found in config")
-    this.peergroup.join(peerId, name)
+    
+    
+    this.ipfs.once('ready', () => this.ipfs.id((err, info) => {
+      if (err) { throw err }
+      console.log('IPFS node ready with address ' + info.id)
+    
+      this.room = Room(this.ipfs, 'ampl-experiment')
+    
+      this.room.on('peer joined', (peer) => {
+        console.log('peer ' + peer + ' joined')
+        if (peer == info.id) { return }
+        this.getOrCreatePeer(peer, peer, undefined)
+      })
+      this.room.on('peer left', (peer) => {
+        console.log('peer ' + peer + ' left')
+        delete this.Peers[peer]
+        // this is wrong (i think?)
+      })
+    
+      // send and receive messages    
+      this.room.on('message', (message) => {
+        console.log('Automerge.Connection> receive ' + message.from + ': ' + message.data.toString())
+        this.connections[message.from].receiveMsg(JSON.parse(message.data.toString()))
+      })
+    }))
 
     this.connected = true
   }
 
+  getOrCreatePeer(id, name, handler) {
+      if (!this.Peers[id]) {
+        this.Peers[id] = name
+        this.connections[id] = new Automerge.Connection(this.docSet, msg => {
+          console.log('Automerge.Connection> send to ' + id + ':', msg)
+          this.room.sendTo(id, JSON.stringify(msg))
+        })
+  
+        this.connections[id].open()
+      }
+      return this.Peers[id]
+  }
+    
   broadcastActiveDocId(docId) {
     // todo: this.webRTCSignaler.broadcastActiveDocId(docId)
   }
@@ -39,7 +88,7 @@ export default class Network extends EventEmitter {
   disconnect() {
     if (this.connected == false) throw "network already disconnected - connect first"
     console.log("NETWORK DISCONNECT")
-    this.peergroup.close()
+    this.ipfs.stop()
     this.connected = false
   }
 }
